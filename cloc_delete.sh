95da735bbd2cb85ae0980cc43d8951949dc0d29b
nkiyo <nkiyo@hoge.com>
2019-05-28 09:22:55 +0900

    Add scripts

---
+++ cloc_delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cloc --diff 3225b88b0c8eaa45d8f7e7fff5d9b866ce04baa5 4c56aa2a407d18dd2da3cecf7f97934c1c6525a8 
+
---
+++ cloc_modify.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cloc --diff 1f015d94c31875bf5454854dfb0de1a5ced86157 3225b88b0c8eaa45d8f7e7fff5d9b866ce04baa5
