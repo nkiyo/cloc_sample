90b430b885ea6b1d5b31d492f99f54c233713f5b
nkiyo <nkiyo@hoge.com>
2019-06-26 08:14:39 +0900

    up

--- fmtjson.js
+++ fmtjson.js
@@ -3,9 +3,9 @@
 // Reads JSON from stdin and writes equivalent
 // nicely-formatted JSON to stdout.
 
-var stdin = process.stdin,
-    stdout = process.stdout,
-    inputChunks = [];
+const stdin = process.stdin
+const stdout = process.stdout
+const inputChunks = []
 
 stdin.resume();
 stdin.setEncoding('utf8');
@@ -15,10 +15,19 @@ stdin.on('data', function (chunk) {
 });
 
 stdin.on('end', function () {
-    var inputJSON = inputChunks.join(),
-        parsedData = JSON.parse(inputJSON),
-        outputJSON = JSON.stringify(parsedData, null, '    ');
-    stdout.write(outputJSON);
-    stdout.write('\n');
+    const inputJSON = inputChunks.join()
+    const parsedData = JSON.parse(inputJSON)
+    //const outputJSON = JSON.stringify(parsedData, null, '    ')
+    let outputJSON
+    for(const key in parsedData) {
+      const val = parsedData[key]
+      //console.log(`### ${key} ${JSON.stringify(val)} hoge`);
+      outputJSON[key] = "hoge"
+      //for(const vkey in val) {
+      //  console.log(`${vkey} hoge`);
+      //}
+    }
+    stdout.write(outputJSON)
+    stdout.write('\n')
 });
 
